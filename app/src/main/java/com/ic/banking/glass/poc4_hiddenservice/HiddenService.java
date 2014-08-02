bcf7de92e506651e887812fae8a8dd85f24dbab2
Mateo Hermida <mateohermida@outlook.com>
2014-08-01 21:05:34 -0300

    agrego proyecto

---
+++ app/src/main/java/com/ic/banking/glass/poc4_hiddenservice/HiddenService.java
@@ -0,0 +1,49 @@
+package com.ic.banking.glass.poc4_hiddenservice;
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.LiveCard.PublishMode;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.RemoteViews;
+
+public class HiddenService extends Service {
+
+    private static final String LIVE_CARD_TAG = "HiddenService";
+
+    private LiveCard mLiveCard;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mLiveCard == null) {
+            mLiveCard = new LiveCard(this, LIVE_CARD_TAG);
+
+            RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.hidden);
+            mLiveCard.setViews(remoteViews);
+
+            // Display the options menu when the live card is tapped.
+            Intent menuIntent = new Intent(this, LiveCardMenuActivity.class);
+            mLiveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
+            mLiveCard.publish(PublishMode.REVEAL);
+        } else {
+            mLiveCard.navigate();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mLiveCard != null && mLiveCard.isPublished()) {
+            mLiveCard.unpublish();
+            mLiveCard = null;
+        }
+        super.onDestroy();
+    }
+}
