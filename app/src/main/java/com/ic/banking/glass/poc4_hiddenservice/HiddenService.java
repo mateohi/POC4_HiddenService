bfe8851140f34250fbc9a917fc46f7707800ad30
Mateo Hermida <mateohermida@outlook.com>
2014-08-06 23:46:28 -0300

    refactoring

--- app/src/main/java/com/ic/banking/glass/poc4_hiddenservice/HiddenService.java
+++ app/src/main/java/com/ic/banking/glass/poc4_hiddenservice/HiddenService.java
@@ -1,21 +1,10 @@
 package com.ic.banking.glass.poc4_hiddenservice;
 
-import com.google.android.glass.timeline.LiveCard;
-import com.google.android.glass.timeline.LiveCard.PublishMode;
-
-import android.app.PendingIntent;
-import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.IBinder;
-import android.widget.RemoteViews;
 
 public class HiddenService extends Service {
-
-    private static final String LIVE_CARD_TAG = "HiddenService";
-
-    private LiveCard liveCard;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -25,29 +14,6 @@ public class HiddenService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         new GraphTask(getApplicationContext()).execute("mateo.hermida");
-        /*
-        if (liveCard == null) {
-            liveCard = new LiveCard(this, LIVE_CARD_TAG);
-
-            RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.hidden);
-            liveCard.setViews(remoteViews);
-
-            // Display the options menu when the live card is tapped.
-            Intent menuIntent = new Intent(this, LiveCardMenuActivity.class);
-            liveCard.setAction(PendingIntent.getActivity(this, 0, menuIntent, 0));
-            liveCard.publish(PublishMode.REVEAL);
-        } else {
-            liveCard.navigate();
-        }*/
         return START_STICKY;
-    }
-
-    @Override
-    public void onDestroy() {
-        if (liveCard != null && liveCard.isPublished()) {
-            liveCard.unpublish();
-            liveCard = null;
-        }
-        super.onDestroy();
     }
 }
